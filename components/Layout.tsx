d0eb21915b2602f8b8d319eb45a09951665bdaab
Wesley Rocha <wesleysr1997@gmail.com>
2022-05-11 01:46:41 -0400

    Make menu prettier

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,8 +3,6 @@ import { FC } from "react";
 import { InfoDialog } from "./InfoDialog";
 
 export const Layout: FC = ({ children }) => {
-  // const { user, session } = Auth.useUser();
-
   return (
     <div className="flex min-h-full min-w-full bg-slate-100">
       <div className="mx-auto flex min-h-full max-w-xl grow flex-col items-center justify-between">
@@ -21,14 +19,5 @@ export const Layout: FC = ({ children }) => {
         {children}
       </div>
     </div>
-    // <div className="mx-auto flex h-screen max-w-3xl flex-col px-4 pt-3">
-    //   {/* <header className="flex justify-between">
-    //     <span className="flex-1">Worlde Arena</span>
-    //     <span>Perfil</span>
-    //     <span>Configuração</span>
-    //   </header> */}
-    //   {/* <span>{JSON.stringify(user, null, 2)}</span> */}
-    //   <main className=" flex flex-grow flex-col">{children}</main>
-    // </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Button = ({ children, className = "", ...props }) => (
     {...props}
     className={classNames(
       className,
-      "flex h-14 w-56 items-center justify-center rounded-md border bg-green-600 px-4 py-2 text-lg font-bold uppercase text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2  disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500"
+      " flex h-16 w-64 items-center justify-center rounded-md border bg-green-600 px-4 py-2 text-2xl font-bold uppercase text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2  disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500"
     )}
   >
     {children}
@@ -21,10 +21,10 @@ const Page = () => {
     <Layout>
       <main className="flex w-full max-w-md grow flex-col items-center justify-center px-12">
         <Button disabled>Jogar online ⚔️</Button>
-        <div className="border-1 absolute ml-48 mb-8 -rotate-12 border-2 border-dotted border-orange-400 bg-orange-100 px-2 py-1 text-sm font-bold uppercase text-orange-400">
+        <div className="border-1 absolute ml-48 mb-8 -rotate-12 border-2 border-dotted border-orange-400 bg-orange-100 px-6 py-1 font-bold uppercase text-orange-400">
           Vem aí
         </div>
-        <Button className="mt-4">Praticar 💪</Button>
+        <Button className="mt-6">Praticar 💪</Button>
       </main>
     </Layout>
   );
